fd43dd9bfc9c51fe97838851daca55edad85bfaa
Mora Ugalde, Jafet <jafet.mora.ugalde@intel.com>
2021-02-25 21:30:07 -0600

    Change the debug mode options for enhance the security.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, redirect, url_for, render_template, request, send_file, jsonify
 from datetime import datetime
 
@@ -10,4 +11,4 @@ def home():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=(os.environ['DEBUG']=='True'))
